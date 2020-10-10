d4255dffe72bd9566a8f998ba2cecfb85d5c24df
Luc Foglizzo <lucfoglizzo@Lucs-MBP.hsd1.ca.comcast.net>
2020-10-09 18:19:20 -0700

    Added some quich fixes and touch ups to remove vs code red error highlighting... am actually impressed i was able to get / fix them all... ! Kudos Claps to me !

--- 03-JavaScript/01-Activities/21-RandomNumbers/Unsolved/app.js
+++ 03-JavaScript/01-Activities/21-RandomNumbers/Unsolved/app.js
@@ -2,20 +2,17 @@
 
 //console.log(Math.floor(Math.random() * 10));
 
-console.log
+console.log;
 
 let choices = ["r", "p", "s"];
 var wins = 0;
 var losses = 0;
 var ties = 0;
-var userChoice = prompt("Please choose r, p, s")
-var computerChoice = choices[Math.floor(Math.random() * choices.length);
+var userChoice = prompt("Please choose r, p, s");
+var computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-
-
-        for (let i = 0; i < choices.length; = [(choices[0], ]
-
-                for (var i = 0; i < choices.length; i++) {;
-
-                    let computerCheck = Math.floor(Math.random() * 3);
-                    if
+for (let i = 0; i < choices.length;
+    [choices[0]])
+    for (var i = 0; i < choices.length; i++) {
+        let computerCheck = Math.floor(Math.random() * 3);
+    }
--- 03-JavaScript/01-Activities/23-RPS-Coded/app.js
+++ 03-JavaScript/01-Activities/23-RPS-Coded/app.js
@@ -2,14 +2,12 @@ let choices = ["r", "p", "s"];
 var wins = 0;
 var losses = 0;
 var ties = 0;
-var userChoice = prompt("Please choose r, p, s")
-var computerChoice = choices[Math.floor(Math.random() * choices.length);
+var userChoice = prompt("Please choose r, p, s");
+var computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
+for (let i = 0; i < choices.length;
+    [choices[0]]);
 
-
-        for (let i = 0; i < choices.length; = [(choices[0], ]
-
-                for (var i = 0; i < choices.length; i++) {;
-
-                    let computerCheck = Math.floor(Math.random() * 3);
-                    if
+for (var i = 0; i < choices.length; i++) {
+    let computerCheck = Math.floor(Math.random() * 3);
+}
