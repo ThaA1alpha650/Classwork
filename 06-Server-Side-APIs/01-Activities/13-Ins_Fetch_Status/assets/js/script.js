dfe30055a0b8933d511351d1572c954f0334f65c
Luc Foglizzo <lucjmfoggy@gmail.com>
2020-11-03 18:44:59 -0800

    Solved Server Side APIs

--- 06-Server-Side-APIs/01-Activities/13-Ins_Fetch_Status/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/13-Ins_Fetch_Status/assets/js/script.js
@@ -3,13 +3,11 @@ var requestUrl = 'https://api.github.com/orgs/nodejs/repos?per_page=5';
 var responseText = document.getElementById('response-text');
 
 function getApi(requestUrl) {
-    fetch(requestUrl)
-        .then(function(response) {
-                console.log(response.status)
-                return response.json)
-            .then(function(data) {
-                console.log(data);
-            });
-        }
+  fetch(requestUrl)
+    .then(function (response) {})
+    .then(function (data) {
+      console.log(data);
+    });
+}
 
-    getApi(requestUrl);
+getApi(requestUrl);
